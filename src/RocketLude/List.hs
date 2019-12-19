5114c715bd72c59a791bb1ae062591f20d53a3e7
JuanGalilea <juangk93@gmail.com>
2019-12-19 12:41:47 -0300

    List con chunksOf

--- src/RocketLude/List.hs
+++ src/RocketLude/List.hs
@@ -1,5 +1,14 @@
 module RocketLude.List 
     ( module Data.List
+    , chunksOf
     ) where
 
+import           RocketLude
 import           Data.List
+
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf k = go
+  where go t = case splitAt k t of
+                (a,b) 
+                    | null a      -> []
+                    | otherwise   -> a : go b
